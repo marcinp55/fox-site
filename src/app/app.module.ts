da3a2d4007e3008165f92eb8ef93d013a1f6e20d
marcinp55 <marcinp55@gmail.com>
2021-06-05 14:16:45 +0200

    Add contact page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
@@ -13,10 +15,12 @@ import { ProductDetailComponent } from './product-detail/product-detail.componen
     HeaderComponent,
     HomeComponent,
     FooterComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GoogleMapsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
